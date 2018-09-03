2d345c4c9a229e5ea94002649766cf6a62124b99
Sean Gregory <seangregory@protonmail.com>
2018-09-03 10:10:50 -0400

    page-factory example

--- features/support/env.js
+++ features/support/env.js
@@ -1,13 +1,11 @@
 const { setWorldConstructor } = require('cucumber')
 const webdriver = require('selenium-webdriver')
 const driver = new webdriver.Builder().forBrowser('chrome').build()
-
-var GoogleSearch = require('./pages/google-search')
-var GoogleResults = require('./pages/google-results')
+const PageFactory = require('screen-pill/page-factory')
 
 function CustomWorld() {
   this.driver = driver
-  this.searchPage = new GoogleSearch(driver)
-  this.resultsPage = new GoogleResults(driver)
+  Object.assign(this, PageFactory)
 }
+
 setWorldConstructor(CustomWorld)
--- features/support/steps/steps.js
+++ features/support/steps/steps.js
@@ -3,15 +3,23 @@ const chai = require('chai')
 chai.use(require('chai-as-promised'))
 const expect = chai.expect
 
+var GoogleSearch = require('./../pages/google-search')
+var GoogleResults = require('./../pages/google-results')
+
+
 Given('I go to google', async function() {
-  await this.searchPage.visit()
+  return this.visit(GoogleSearch)
 })
 
 When(/I search (.*)/, async function(term) {
-  await this.searchPage.query(term)
+  return this.on(GoogleSearch, async function(page) {
+    await page.query(term)
+  })
 })
 
 Then('I get results', async function() {
-  await this.resultsPage.stats.waitUntilPresent()
-  expect(this.resultsPage.stats.get()).to.eventually.match(/About/)
+ return this.on(GoogleResults, async function(page) {
+    await page.stats.waitUntilPresent()
+    expect(page.stats.get()).to.eventually.match(/About/)
+  })
 })
